0f86cb1b0aec6061ec1b14d541621682e4468bb4
Jenish S Patel <jenishsomabhaipatel@gmail.com>
2026-06-15 15:18:50 +0530

    Add functions to change background color randomly

--- Generate_Random_Background/script.js
+++ Generate_Random_Background/script.js
@@ -1,1 +1,24 @@
+const randomColor = function(){
+            const hex = "0123456789ABCDEF";
+            let color = '#';
+            for(let i=0; i<6; i++){
+                color += hex[Math.floor(Math.random()*16)];
+            }
+            return color;
+        }
+        let intervalId;
+        const startchangingcolor = function(){
+            if(!intervalId){
+                intervalId = setInterval(changeBg, 2000);
+            }
+            function changeBg(){
+                document.body.style.backgroundColor = randomColor();
+            }
+        }
+        const stopchangingcolor = function(){
+            clearInterval(intervalId);
+            intervalId = null;
+        }
+        document.getElementById("start").addEventListener("click",startchangingcolor);
 
+        document.getElementById("stop").addEventListener("click",stopchangingcolor);
